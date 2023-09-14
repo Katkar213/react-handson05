cf624bde3c377898ba056e7f90f062325e622d10
katkar213 <blackship213@gmail.com>
2023-09-14 16:57:06 +0530

    just commit

--- src/App.js
+++ src/App.js
@@ -17,8 +17,16 @@ function App() {
      <PureCompo/>
 
      <br/>
-     <h4><span style={{color:"red"}}>Explaination :</span>  In HOC the text in text area changes to upper case by clicking on button and later by HOC we can change color of one component<br/>
-     and in Pure Componemt I have change vlue from 1 to 2 by ending that variable at 2 console stops rerendering i.e console render only once.</h4>
+
+     <h1>Hoc:</h1>
+     <p>Higher-order components (HOCs) are a powerful feature of the React library. They allow you to reuse component logic across multiple components. In React, a higher-order component is a function that takes a component as an argument and returns a new component that wraps the original component.</p>
+     
+     <h1>PureComponent:</h1>
+     <p>PureComponent is similar to Component but it skips re-renders for same props and state. Class components are still supported by React, but we don’t recommend using them in new code.</p>
+     
+
+     <h4><span style={{color:"red"}}>Explaination :</span>IN above example In HOC the text in text area changes to upper case by clicking on button and later by HOC we can change color of one component by using single HOC component we can change these multiple time.<br/>
+     and in Pure Componemt I have change vlue from 1 to 2 by ending that variable at 2 console stops rerendering i.e console render only once.and it stops re rendering cod at specific interval</h4>
     </div>
   );
 }
